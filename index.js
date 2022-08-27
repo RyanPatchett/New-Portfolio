befe80a5d2dda7577eea0301236efc2e2005f421
Ryan <ryan_patchett@outlook.com>
2022-08-27 16:27:48 +0100

    Added background shapes and on-cursor-move animations

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-//template_dwc9kxu
-//service_uutw0nr
-//3QVFikVlMQMx6lU4E
+const scaleFactor = 1 / 20
 
 function contact(event){
     event.preventDefault();
@@ -32,4 +30,18 @@ function toggleModal() {
 
 function toggleContrast(){
     document.body.classList.toggle("dark-theme")
+}
+
+function moveBackground(event){
+    const shapes = document.querySelectorAll(".shape")
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (let i = 0; i < shapes.length; i++) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px,${y * boolInt}px)`
+        
+    }
+
 }
